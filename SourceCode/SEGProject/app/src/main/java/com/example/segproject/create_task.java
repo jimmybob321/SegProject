d654bbe1ed4f1ec6dade60af9daae2792b84db44
Jason Morrill <jasonm321gm@gmail.com>
2017-11-25 15:06:32 -0500

    added UI implementation
    For Create Task

--- SourceCode/SEGProject/app/src/main/java/com/example/segproject/create_task.java
+++ SourceCode/SEGProject/app/src/main/java/com/example/segproject/create_task.java
@@ -1,25 +1,53 @@
 package com.example.segproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class create_task extends AppCompatActivity {
-
+    EditText TaskName;
+    EditText TaskDate;
+    EditText TaskPriority;
+    EditText TaskReward;
+    EditText TaskUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TaskName = (EditText) findViewById(R.id.txtName);
+        TaskDate = (EditText) findViewById(R.id.txtDate);
+        TaskPriority = (EditText) findViewById(R.id.txtReward);
+        TaskReward = (EditText) findViewById(R.id.txtPriority);
+        TaskUser = (EditText) findViewById(R.id.txtAssignUser);
 
 
 
 
     }
+    public void CreateTaskClick(View view){
+        TaskName = (EditText) findViewById(R.id.txtName);
+        TaskDate = (EditText) findViewById(R.id.txtDate);
+        TaskPriority = (EditText) findViewById(R.id.txtReward);
+        TaskReward = (EditText) findViewById(R.id.txtPriority);
+        TaskUser = (EditText) findViewById(R.id.txtAssignUser);
+        createTask(TaskName.getText().toString().trim(),TaskDate.getText().toString().trim(),Integer.parseInt(TaskPriority.getText().toString().trim()),Integer.parseInt(TaskReward.getText().toString().trim()));
+    }
+    private void createTask(String name, String Date, Integer priority, Integer reward){
+        //implement creating task
 
+
+
+
+        Intent returnIntent = new Intent();
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
 }
