47d8cef3b75b6db29c0e79818a89716088378ed2
Jason Morrill <jasonm321gm@gmail.com>
2017-11-25 13:26:13 -0500

    Added onclick for login

--- SourceCode/SEGProject/app/src/main/java/com/example/segproject/LoginPage.java
+++ SourceCode/SEGProject/app/src/main/java/com/example/segproject/LoginPage.java
@@ -2,12 +2,28 @@ package com.example.segproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
-
+    Button btnLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        btnLoginButton = (Button) findViewById(R.id.btnLogin);
+        btnLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login();
+            }
+        });
     }
+    private void Login(){
+        //james implement this method
+    }
+
 }
