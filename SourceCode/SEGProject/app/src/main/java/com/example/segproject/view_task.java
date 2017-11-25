62c62ac79e39ddd2eddf3b3f62079917e0235e8b
Jason Morrill <jasonm321gm@gmail.com>
2017-11-25 15:18:42 -0500

    Added UI Implementation
    For View Task

--- SourceCode/SEGProject/app/src/main/java/com/example/segproject/view_task.java
+++ SourceCode/SEGProject/app/src/main/java/com/example/segproject/view_task.java
@@ -1,5 +1,6 @@
 package com.example.segproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,18 +14,23 @@ public class view_task extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_task);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
+    public void CompleteClick(View view){
+        completeTask();
+    }
+    public void DeclineClick(View view){
+        declineTask();
+    }
+    public void ReturnClick(View view){
+        Intent returnIntent = new Intent();
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+    private void completeTask(){
+        // needs Implementation
+    }
+    private void declineTask(){
+        // Needs Implementation
+    }
 }
