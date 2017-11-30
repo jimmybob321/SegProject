565f98c0997548f0a87d7d01115bbacb560d6ea4
Jason Morrill <jasonm321gm@gmail.com>
2017-11-30 13:18:55 -0500

    added some implementation and pseudocode

--- SourceCode/SEGProject/app/src/main/java/com/example/segproject/view_task.java
+++ SourceCode/SEGProject/app/src/main/java/com/example/segproject/view_task.java
@@ -9,39 +9,35 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class view_task extends AppCompatActivity {
-
+    Task T;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_task);
-
+        T = new Task("",1,"",1);//blank to not cause errors we still need to figure out how to pass an object to this page
     }
     public void CompleteClick(View view){
-        completeTask();
+        completeTask(T);
     }
     public void DeclineClick(View view){
-        declineTask();
+        declineTask(T);
     }
     public void ReturnClick(View view){
         Intent returnIntent = new Intent();
         setResult(RESULT_OK, returnIntent);
         finish();
     }
-    private void completeTask(){
-        // needs Implementation
+    private void completeTask(Task compTask){
 
-
-
-
+        int scoreToAdd = compTask.getReward();
+        //Profile.set_Score(Profile.get_score + scoreToAdd);
         Intent returnIntent = new Intent();
         setResult(RESULT_OK, returnIntent);
         finish();
     }
-    private void declineTask(){
-        // Needs Implementation
-
-
-
+    private void declineTask(Task delTask){
+        //Profile.RemoveTask(delTask);
+        //ProfileUnassigned.AddTask(delTask);
         Intent returnIntent = new Intent();
         setResult(RESULT_OK, returnIntent);
         finish();
