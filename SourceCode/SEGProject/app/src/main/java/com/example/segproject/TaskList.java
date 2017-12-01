36e0817174c141fc2f38851047841d0e50f718e6
Jarod Lee <jlee431@uottawa.ca>
2017-12-01 14:00:21 -0500

    Working on displaying Tasks

---
+++ SourceCode/SEGProject/app/src/main/java/com/example/segproject/TaskList.java
@@ -0,0 +1,41 @@
+package com.example.segproject;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.List;
+
+/**
+ * Created by Jarod Lee on 2017-12-01.
+ */
+
+public class TaskList extends ArrayAdapter<Task> {
+    private Activity context;
+    List<Task> tasks;
+
+    public TaskList(Activity context, List<Task> tasks) {
+        super(context, R.layout.layout_task_list, tasks);
+        this.context = context;
+        this.tasks = tasks;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.layout_task_list, null, true);
+
+        TextView txtName = (TextView) listViewItem.findViewById(R.id.txtName);
+        TextView txtDate = (TextView) listViewItem.findViewById(R.id.txtDate);
+        TextView txtReward = (TextView) listViewItem.findViewById(R.id.txtReward);
+
+        Task task = tasks.get(position);
+        txtName.setText(task.getTitle());
+        txtDate.setText(task.getDate());
+        txtReward.setText(task.getReward());
+        return listViewItem;
+    }
+
+}
--- SourceCode/SEGProject/app/src/main/java/com/example/segproject/child_profile.java
+++ SourceCode/SEGProject/app/src/main/java/com/example/segproject/child_profile.java
@@ -8,15 +8,29 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class child_profile extends AppCompatActivity {
     Profile USER;
     TextView name, score;
     ImageView avatar;
+    ListView listViewTasks;
+    ArrayList<Task> tasks;
+    DatabaseReference dR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +38,54 @@ public class child_profile extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         USER = (Profile) getIntent().getSerializableExtra("Profile");
         avatar = (ImageView) findViewById(R.id.imgAvatar);
         name = (TextView) findViewById(R.id.txtName);
         score = (TextView) findViewById(R.id.txtScore);
+        listViewTasks = (ListView) findViewById(R.id.listViewTasks);
+        tasks = new ArrayList<>();
 
         name.setText(new StringBuilder("Name: ").append(USER.get_name()));
         score.setText(new StringBuilder("Score: ").append(USER.get_score()));
         int resID = getResources().getIdentifier(USER.get_img(), "drawable", getPackageName());
         avatar.setImageResource(resID);
+
+        dR = FirebaseDatabase.getInstance().getReference("tasks").child(USER.get_name());
+
+        listViewTasks.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Task task = tasks.get(i);
+                return true;
+            }
+        });
+
     }
-        //TODO add display of tasks
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dR.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snap) {
+                tasks.clear();
+
+                for (DataSnapshot postSnapshot : snap.getChildren()) {
+                    Task task = postSnapshot.getValue(Task.class);
+                    tasks.add(task);
+                }
+
+                //adapter
+                TaskList tasksAdapter = new TaskList(child_profile.this, tasks);
+                listViewTasks.setAdapter(tasksAdapter);
+            }
+            @Override
+            public  void onCancelled(DatabaseError error){
+            }
+        });
+    }
+
+    //TODO add display of tasks
 
 }
