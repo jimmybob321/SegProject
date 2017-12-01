de3b142d56b406125862d2e84d797e31a2032f1b
Jarod Lee <jlee431@uottawa.ca>
2017-12-01 02:37:45 -0500

    Created calendar picker dialog for create task

--- SourceCode/SEGProject/app/src/main/java/com/example/segproject/create_task.java
+++ SourceCode/SEGProject/app/src/main/java/com/example/segproject/create_task.java
@@ -1,15 +1,16 @@
 package com.example.segproject;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 
 public class create_task extends AppCompatActivity {
@@ -19,6 +20,10 @@ public class create_task extends AppCompatActivity {
     EditText TaskReward;
     EditText TaskUser;
     Profile USER;
+    DatePicker datePicker;
+    Calendar calendar;
+    private int year, month, day;
+
     Task T;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +36,60 @@ public class create_task extends AppCompatActivity {
         TaskUser = (EditText) findViewById(R.id.txtAssignUser);
         USER = (Profile) getIntent().getSerializableExtra("Profile");
 
+        TaskDate.setInputType(0);
+
+
         if (!USER.isParent())
             TaskUser.setInputType(0); //Disables TaskUser if user is not a parent.
 
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
     }
+
+
+    public void onClickDate(View view) {
+            showDialog(999);
+        }
+
+            @Override
+            protected Dialog onCreateDialog(int id) {
+                if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        TaskDate.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+
+
     public void CreateTaskClick(View view){
-        TaskName = (EditText) findViewById(R.id.txtName);
-        TaskDate = (EditText) findViewById(R.id.txtDate);
-        TaskPriority = (EditText) findViewById(R.id.txtReward);
-        TaskReward = (EditText) findViewById(R.id.txtPriority);
-        TaskUser = (EditText) findViewById(R.id.txtAssignUser);
-        if(TaskUser.getText().toString().trim() != "") {
+        if(!TaskUser.getText().toString().trim().equals("")) {
             createTask(TaskName.getText().toString().trim(), TaskDate.getText().toString().trim(), Integer.parseInt(TaskPriority.getText().toString().trim()), Integer.parseInt(TaskReward.getText().toString().trim()));
         }
         else{
             AssignNewTask(TaskName.getText().toString().trim(), TaskDate.getText().toString().trim(), Integer.parseInt(TaskPriority.getText().toString().trim()), Integer.parseInt(TaskReward.getText().toString().trim()),TaskUser.getText().toString().trim());
         }
-        }
+    }
 
     private void createTask(String name, String Date, int priority, int reward){
         T = new Task(name,reward,Date,priority);
@@ -56,8 +98,8 @@ public class create_task extends AppCompatActivity {
         setResult(RESULT_OK, returnIntent);
         finish();
     }
-    private void AssignNewTask(String name, String Date, int priority, int reward, String user){
-        T = new Task(name,reward,Date,priority);
+    private void AssignNewTask(String name, String Date, int priority, int reward, String user) {
+        T = new Task(name, reward, Date, priority);
         //TODO find child user once database is fixed
         //get user
         //USERgiven.addTask(T);
